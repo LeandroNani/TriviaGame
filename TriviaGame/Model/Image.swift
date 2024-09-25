1c1dcbe38d80557f383deca599f2742a21fdaffd
Leandro Pacheco <leandropacheco02@hotmail.com>
2024-09-25 15:55:28 -0300

    docs: models feitas

--- TriviaGame/Model/Image.swift
+++ TriviaGame/Model/Image.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+
+//Declarando uma propriedade constante chamada urls dentro da struct ImageModel para que uma vez que o valor de urls for definido quando um ImageModel for criado, ele não poderá ser alterado.
+struct ImageModel: Decodable {
+    let urls: [String: String] // defini um dicionário para a estrutura, que receberá uma string indicando o tamanho ("small", "medium", "raw", "full") como chave, e uma string que será a url da imagem de tamanho correspondente
+}
--- TriviaGame/Model/TriviaQuestion.swift
+++ TriviaGame/Model/TriviaQuestion.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+//Decodable é um protocolo (como um conjunto de regras) em Swift que permite que você converta dados em formato JSON (que você recebe de uma API) para objetos Swift que você pode usar no seu código. Pense nisso como uma "tradução" de dados.
+
+// Identifiable é outro protocolo usado quando quero exibir listas de dados em SwiftUI (mas também pode ser útil em outros contextos). Ele basicamente diz: "Cada objeto nesta lista tem uma maneira de ser identificado unicamente". Isso é importante pois quando um item mudar, a lista saberá qual item atualizar, sem precisar recarregar a lista toda novamente. Para utilizar eu só preciso fornecer uma propriedade única para cada objeto, como um id numérico que recebo, ou um id UUID gerado automaticamnente
+
+struct TriviaQuestion: Decodable, Identifiable { // Ao declarar minha struct como DECODABLE eu sinalizo que ela sabe interpretar os dados json que se assimilarem com o que eu defini. Então o proprio swift usa as propriedades da struct (category, type, difficulty...) para mapear o json recebido e criar um objeto
+    let id: Int // Um ID unico pra cada pergunta
+    let category: String
+    let type: String
+    let difficulty: String
+    let question: String
+    let correct_answer: String
+    let incorrect_answers: [String]
+}
