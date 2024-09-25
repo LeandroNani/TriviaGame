f6d863be0b7ecc5ec2432a4bf04190992846d1fe
Leandro Pacheco <leandropacheco02@hotmail.com>
2024-09-25 14:48:12 -0300

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by dtidigital on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
